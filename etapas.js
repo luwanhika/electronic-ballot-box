3f5b988a3f5a190498bff9475955e0cccd3cace9
luwanhika <luwanhika@hotmail.com>
2021-06-17 22:27:44 -0300

    criar etapas

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: 38111,
+        nome: 'Sulano de jé',
+        partido: 'ABC',
+        fotos: [
+          {url: '38111.jpeg', legenda: 'Vereador', small: false}
+        ]
+      },
+      {
+        numero: 77222,
+        nome: 'Sicrano da Silva',
+        partido: 'DEFG',
+        fotos: [
+          {url: '77222.jpeg', legenda: 'Vereador'}
+        ]
+      },
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 99,
+        nome: 'Beltrano',
+        partido: 'ABC',
+        vice: 'Nombrega',
+        fotos: [
+          {url: '99.jpeg', legenda: 'Prefeito'},
+          {url: '99_2.png', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+      {
+        numero:84,
+        nome: 'Troiano',
+        partido: 'QWERT',
+        vice: 'Grego',
+        fotos: [
+          {url: '84.jpeg', legenda: 'Prefeito'},
+          {url: '84_2.jpeg', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+    ]
+  }
+];
