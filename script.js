f914c5161f892b8eddbadd33658725c19ab46cad
luwanhika <luwanhika@hotmail.com>
2021-06-22 12:40:23 -0300

    finalizando votos

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let numeros = document.querySelector('.d-1-3');
 let etapaAtual = 0;
 let numero = '';
 let votoBranco = false;
+let votos = [];
 
 function comecarEtapa() {
   let etapa = etapas[etapaAtual];
@@ -96,10 +97,17 @@ function confirma() {
 
   if(votoBranco === true) {
     votoConfirmado = true;
+    votos.push({
+      etapa: etapas[etapaAtual].titulo,
+      voto: 'branco'
+    });
     console.log("Confirmando como BRANCO...");
   } else if(numero.length === etapa.numeros) {
     votoConfirmado = true;
-    console.log("Confirmando como "+numero);
+    votos.push({
+      etapa: etapas[etapaAtual].titulo,
+      voto: numero
+    });
   }
 
   if(votoConfirmado) {
@@ -107,7 +115,8 @@ function confirma() {
     if(etapas[etapaAtual] !== undefined) {
       comecarEtapa();
     } else {
-      console.log("FIM");
+      document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>';
+      console.log(votos);
     }
   }
 }
